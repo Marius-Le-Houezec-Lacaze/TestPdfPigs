8bb92a2a9772e1cc22865881cfcd5887c900a59c
Marius Le Houezec Lacaze <mlehouezeclacaze@lucca.fr>
2024-02-29 16:34:00 +0100

    init

---
+++ TestPdfPigs/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var document = PdfDocument.Open(@"C:\Test pdfs\test.pdf"))
+        {
+            foreach (var page in document.GetPages())
+            {
+                var words = page.GetWords();
+
+                foreach (var word in words)
+                {
+                    Console.WriteLine(word.Text);
+                    Console.WriteLine(word.BoundingBox.Right);
+                    Console.WriteLine(word.BoundingBox.Left);
+                }
+                
+            }
+        }
+    }
+}
